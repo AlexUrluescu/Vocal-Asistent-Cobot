73ecea76712dffdb9c99d1eda98f2ec9775a9b2c
AlexUrluescu <urluescualex2003@gmail.com>
2023-07-01 18:05:40 +0300

    Create methods and Bus connection to the Cobot

--- task.py
+++ task.py
@@ -11,17 +11,19 @@ import requests
 import os.path
 from datetime import date, datetime
 import logging
+# from vocal_assistant import Assistent
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+
 
 class Task():
 
     def __init__(self, assistant_name):
-        self.task_list = [assistant_name, "open my presentation", "next", "previous", "weather", "time", "date", 'thank you', "minimize", "close", "open", "focus", "maximize", "mode", "finish", "say", "goodbye"]
+        self.task_list = [assistant_name, "hey", "mouse", "apple", "goodbye"]
         self.assistant_name = assistant_name
         self.keyboard = Controller()
 
-        self.task_list2 = [assistant_name, "open my presentation", "next", "previous", "weather"]
+        # self.vocal_asistent = Assistent()
         
 
     def identify_task(self, text: str) -> int:
@@ -33,328 +35,23 @@ class Task():
                 return task_index
         
         return -127
-    
-       
-    def loadwindowslist(self, hwnd, topwindows):
-        topwindows.append((hwnd, win32gui.GetWindowText(hwnd)))
-        
-    
-    def findandshowwindow(self, swinname, bshow, bbreak):
-        topwindows = []
-
-        win32gui.EnumWindows(self.loadwindowslist, topwindows)
-
-        for hwin in topwindows:
-            sappname = str(hwin[1])
-            if swinname in sappname.lower():
-                nhwnd = hwin[0]
-                logging.debug(type(nhwnd))
-                logging.debug(">>> Found: " + str(nhwnd) + ": " + sappname)
-                if(bshow):
-                    win32gui.ShowWindow(nhwnd, 5)
-                    shell = win32com.client.Dispatch("WScript.Shell")
-                    shell.SendKeys('%')
-                    win32gui.SetForegroundWindow(nhwnd)
-                if(bbreak):
-                    break
 
 
-    def focus(self, string):
-        string = string.split(" ")[-1]
-        lista = []
-
-        windows = Desktop(backend="uia").windows()
-
-        for w in windows:
-            lista.append(w.window_text().lower())
-            logging.debug(w.window_text())
-
-        logging.debug(f"{lista} nr 1")
-        logging.debug(string)
-
-        # ---------- change the name of word and powerpoint -----------
-        i=0
-
-        while i < len(lista):
-            logging.debug("a intrat in while")
-            if("powerpoint" in lista[i]):
-                lista[i] = "powerpoint"
-                logging.debug("am gasit powerpoint")
-
-            if("word" in lista[i]):
-                lista[i] = "word"
-                logging.debug("am gasit word")
-
-            if("visual studio code" in lista[i]):
-                lista[i] = "code"
-                logging.debug("am gasit visual studio code")
-
-            if("edge" in lista[i]):
-                lista[i] = "microsoft"
-                logging.debug("am gasit edge")
-
-            if("WhatsApp" in lista[i]):
-                lista[i] = "whatsapp"
-                logging.debug('am gasit whatsapp')
-        
-            i=i+1
-
-        # ---------------------------------------------------
-
-        logging.debug(f"{lista} nr 2")
-
-        if string in lista:
-            self.findandshowwindow(string, True, True)
-            logging.debug(f"{string} exist")
-
-        else:
-            logging.debug("doesn't exist")
-
-    
-    def open(self, string):
-        
-        string = string.split(" ")[-1]
-        logging.debug("se executa functia open")
-
-        if(string == "powerpoint"):
-            logging.debug("a intrat in powerpoint")
-            os.system("start powerpnt")
-
-        if(string == "word"):
-            logging.debugnt("a intrat in word")
-            os.system("start winword")
-
-        if(string == "microsoft"):
-            logging.debug("a intrat in microsoft")
-            os.system("start msedge")
-
-        else:
-            logging.debug(f"a intrat in {string}")
-            run(string)
-            logging.debug("notepad")
-
-
-    def minimise(self):
-        pyautogui.keyDown("win")
-        pyautogui.keyDown("down")
-        pyautogui.keyUp("down")
-        pyautogui.keyUp("win")   
+    def hei(self):
+        print("Hey")
+        # self.vocal_asistent.speak("Hey")
     
 
-    def maximise(self):
-        pyautogui.keyDown("win")
-        pyautogui.keyDown("up")
-        pyautogui.keyUp("up")
-        pyautogui.keyUp("win")
-
-
-    def open_my_presentation(self):
-        logging.debug("open_mt_presentation is activ")
-
-        path = "C:\projectsPowerPoint"
-        files = os.listdir(path)
-        lista = []
-
-        for file in files:
-            size = len(file)
-            file = file[:size - 5]
-            logging.debug(file)
-            if "_" in file:
-                file = file.replace("_", " ")
-            lista.append(file)
-
-        logging.debug(files)
-        logging.debug(lista)
-
-        return lista, path, files
-    
-
-    def presentation_mode(self):
-        pyautogui.keyDown("win")
-        pyautogui.keyDown("f5")
-        pyautogui.keyUp("f5")
-        pyautogui.keyUp("win")
+    def test(self):
+        print("Mouse")
+        # self.vocal_asistent.speak("Mouse")
 
     
-    def finish_presentation(self):
-        pyautogui.keyDown("esc")
-        pyautogui.keyUp("esc")
-        logging.debug("finish a mers")
-
-    
-    def close_file(self):
-        logging.debug("Se executa functia close_file")
-        sleep(1)
-
-        # combination = {Key.alt, Key.f4}
-        logging.debug("se inchide")
-        self.keyboard.press(Key.alt)
-        self.keyboard.press(Key.f4)
-        self.keyboard.release(Key.alt)
-        self.keyboard.release(Key.f4)
-        logging.debug("s-a inchis")
-
-    
-    def next_slide(self):
-        logging.debug("Se executa functia next_slide")
-        # sleep(1)
-        
-        self.keyboard.press(Key.down)
-        self.keyboard.release(Key.down)
-
-
-    def previous_slide(self):
-        logging.debug("Se executa functia previous_slide")
-        # sleep(1)
-        self.keyboard.press(Key.up)
-        self.keyboard.release(Key.up)
-
-    
-    def weather(self, string):
-        try:
-
-            city = string.split(" ")[-1]
-            logging.debug(f"Weather in {city}")
-
-            api_key = "55c8bfaf9fff464f3bf6f3c283186dc6"
-            weather_data = requests.get(
-                    f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
-                
-            weather_data.raise_for_status()
-        
-            weather_data = weather_data.json()
-
-            logging.debug(weather_data)
-            
-            return weather_data
-
-        except requests.exceptions.RequestException as error:
-            logging.debug(f'HTTP error occurred: {error}')
-
-        except Exception as error:
-            logging.debug(f'Other error occurred: {error}')
-
-    
-    def time(self):
-        logging.debug("Se executa functia time")
-        now = datetime.now()
-        ora = now.strftime("%H:%M")
-
-        return ora
+    def apple(self):
+        print("Apple")
+        # self.vocal_asistent.speak("Apple")
     
 
-    def date(self):
-        logging.debug("Se executa functia date")
-        data = date.today()
-
-        return data
-
-    
-    def thank_you(self):
-        logging.debug("Se executa functia thank you")
-
-    
-    def say_something(self):
-        logging.debug("Se executa functia say")
-        run("notepad")
-
-        sleep(2)
-
-        pyautogui.keyDown("T")
-        pyautogui.keyUp("T")
-        pyautogui.keyDown("h")
-        pyautogui.keyUp("h")
-        pyautogui.keyDown("a")
-        pyautogui.keyUp("a")
-        pyautogui.keyDown("n")
-        pyautogui.keyUp("n")
-        pyautogui.keyDown("k")
-        pyautogui.keyUp("k")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("y")
-        pyautogui.keyUp("y")
-        pyautogui.keyDown("o")
-        pyautogui.keyUp("o")
-        pyautogui.keyDown("u")
-        pyautogui.keyUp("u")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("f")
-        pyautogui.keyUp("f")
-        pyautogui.keyDown("o")
-        pyautogui.keyUp("o")
-        pyautogui.keyDown("r")
-        pyautogui.keyUp("r")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("w")
-        pyautogui.keyUp("w")
-        pyautogui.keyDown("a")
-        pyautogui.keyUp("a")
-        pyautogui.keyDown("t")
-        pyautogui.keyUp("t")
-        pyautogui.keyDown("c")
-        pyautogui.keyUp("c")
-        pyautogui.keyDown("h")
-        pyautogui.keyUp("h")
-        pyautogui.keyDown("i")
-        pyautogui.keyUp("i")
-        pyautogui.keyDown("n")
-        pyautogui.keyUp("n")
-        pyautogui.keyDown("g")
-        pyautogui.keyUp("g")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("m")
-        pyautogui.keyUp("m")
-        pyautogui.keyDown("e")
-        pyautogui.keyUp("e")
-        pyautogui.keyDown(".")
-        pyautogui.keyUp(".")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("I")
-        pyautogui.keyUp("I")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("s")
-        pyautogui.keyUp("s")
-        pyautogui.keyDown("e")
-        pyautogui.keyUp("e")
-        pyautogui.keyDown("e")
-        pyautogui.keyUp("e")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("y")
-        pyautogui.keyUp("y")
-        pyautogui.keyDown("o")
-        pyautogui.keyUp("o")
-        pyautogui.keyDown("u")
-        pyautogui.keyUp("u")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("n")
-        pyautogui.keyUp("n")
-        pyautogui.keyDown("e")
-        pyautogui.keyUp("e")
-        pyautogui.keyDown("x")
-        pyautogui.keyUp("x")
-        pyautogui.keyDown("t")
-        pyautogui.keyUp("t")
-        pyautogui.keyDown(" ")
-        pyautogui.keyUp(" ")
-        pyautogui.keyDown("y")
-        pyautogui.keyUp("y")
-        pyautogui.keyDown("e")
-        pyautogui.keyUp("e")
-        pyautogui.keyDown("a")
-        pyautogui.keyUp("a")
-        pyautogui.keyDown("r")
-        pyautogui.keyUp("r")
-        pyautogui.keyDown("!")
-        pyautogui.keyUp("!")
-
-    
     def goodbye(self):
-        logging.debug("Se executa functia goodbye")
+        print("Goodbye")
+        # self.vocal_asistent.speak("Goodbye")
--- vocal_assistant.py
+++ vocal_assistant.py
@@ -10,6 +10,16 @@ from datetime import date, datetime
 import webbrowser
 import logging
 
+from pymodbus.client import ModbusTcpClient
+
+# Adresa IP și portul serverului Modbus TCP/IP
+SERVER_IP = '192.168.0.40'
+SERVER_PORT = 502
+
+# Creează un client Modbus TCP/IP
+client = ModbusTcpClient(SERVER_IP, SERVER_PORT)
+
+
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
 class Assistent():
@@ -54,156 +64,103 @@ class Assistent():
                 # self.speak("Sorry, could not undersand you")
 
         return said
-
-        
-    def change_name(self, new_name):
-        self.name = new_name
-        return "Now the assistent's name it's " + new_name
     
     
     def get_name(self):
         return self.name
     
-    
-    def change_gender(self, new_gender):
-        self.gender = new_gender
-        logging.debug(f"The gender of the assitent was changed to {new_gender}")
-    
-    
-    def get_gender(self):
-        logging.debug(f"The gender of the assistent {self.name} is {self.gender}")
-        return self.gender
-    
-    
-    def change_speech_speed(self, new_speed):
-        self.speech_speed = new_speed
-        self.engine_speak.setProperty('rate', self.speech_speed)
-        logging.debug(f"The speed was changet to  + {new_speed}")
-    
-    
-    def get_speech_speed(self):
-        logging.debug(f"The speed is {self.speech_speed}")
-        return self.speech_speed
-    
 
     def run_task(self, task_index, text):
         if task_index == 0:
             self.speak('What can I do for you?')
+
         
         if task_index == 1:
-            lista, path, files = self.task_manager.open_my_presentation()
-            size = len(lista)
-            leter = "C"
+            client.connect()
 
-            if size == 1:
-                file = lista[0]
-                self.speak(f"You have {size} file in the folder")
-                self.speak(f"The name of the file is {file}")
-                os.system(f"start powerpnt /{leter} {path}/{files[0]}")
-
+            # Scrierea unei valori în registrul de ieșire
+            result = client.write_registers(address= 130, values= 1, unit=1)
+            if result.isError():
+                print('Eroare:', result)
             else:
-                i=1
-                self.speak(f"You have {size} files in the folder")
-                for project in list:
-                    self.speak(f"Say {i} for open {project}")
-                    i = i + 1
+                print('Valori scrise cu succes.')
 
-            text = self.get_audio()
-            print(type(text))
+            while(result == 0):
+                result = client.read_input_registers(address=134, count=1, unit=1)
+                if result.isError():
+                    print('Eroare:', result)
+                else:
+                    print('Valoare citită:', result.registers)
+
+
+            client.close()
             
-            if text == "1":
-                self.speak(f"I will open {lista[0]}")
-                os.system(f"start powerpnt /{leter} {path}/{files[0]}")
-            
-            if text == "2":
-                self.speak(f"I will open {lista[1]}")
-                os.system(f"start powerpnt /{leter} {path}/{files[1]}")
+
+            # here you will put the method ----------------------------
+            # self.task_manager.hei()
+
+
+            # ---------------------------------------------------------
 
         
         if task_index == 2:
-            self.task_manager.next_slide()
+
+            client.connect()
+
+            # Scrierea unei valori în registrul de ieșire
+            result = client.write_registers(address= 130, values= 2, unit=1)
+            if result.isError():
+                print('Eroare:', result)
+            else:
+                print('Valori scrise cu succes.')
+
+            while(result == 0):
+                result = client.read_input_registers(address=134, count=1, unit=1)
+                if result.isError():
+                    print('Eroare:', result)
+                else:
+                    print('Valoare citită:', result.registers)
+
+            
+            
+            client.close()
+
+            # here you will put the method ----------------------------
+            # self.task_manager.test()
+
+
+            # ---------------------------------------------------------
+
         
         if task_index == 3:
-            self.task_manager.previous_slide()
+            # client.connect()
 
+            # # Scrierea unei valori în registrul de ieșire
+            # result = client.write_registers(address= 130, values=3, unit=1)
+            # if result.isError():
+            #     print('Eroare:', result)
+            # else:
+            #     print('Valori scrise cu succes.')
+
+            # while(result == 0):
+            #     result = client.read_input_registers(address=134, count=1, unit=1)
+            #     if result.isError():
+            #         print('Eroare:', result)
+            #     else:
+            #         print('Valoare citită:', result.registers)
+
+
+            
+            client.close()
+
+            # here you will put the method ----------------------------
+            self.task_manager.apple()
+            self.speak("Apple")
+
+
+
+            # ---------------------------------------------------------
 
         if task_index == 4:
-            string = text.split(" ")[-1]
-
-            weather_data = self.task_manager.weather(text)
-
-
-            if(weather_data):
-
-                temperature = round(int(((weather_data['main']['temp'])-32) * 5) / 9)
-                main = weather_data['weather'][0]['main']
-                if(main == "Clouds"):
-                        main = "cloudy"
-
-                elif(main == "Mist"):
-                        main = "misty"
-
-                elif(main == "Fog"):
-                        main = "fogy"
-
-                self.speak(f"In {string} the weather is {main}")
-                sleep(0.5)
-                self.speak(f"and are {temperature} degrees")
-            
-            else:
-                self.speak("Sorry, I dont't find the city")
-
-
-        if task_index == 5:
-            ora = self.task_manager.time()
-            self.speak(ora)
-
-
-        if task_index == 6:
-            data = self.task_manager.date()
-            self.speak(data)
-
-
-        if task_index == 7:
-            self.speak('You welcome')
-            
-
-        if task_index == 8:
-            self.speak("I will minimize the window")
-            self.task_manager.minimise()
-            
-
-        if task_index == 9:
-            self.speak("I will close the window")
-            self.task_manager.close_file()
-
-      
-        if task_index == 10:
-            self.speak("I will open the file")
-            self.task_manager.open(text)
-
-
-        if task_index == 11:
-            self.speak("Focus active")
-            self.task_manager.focus(text)
-
-
-        if task_index == 12:
-            self.speak("I will maximize the window")
-            self.task_manager.maximise()
-
-        if task_index == 13:
-            self.speak("I will active the presentation mode")
-            self.task_manager.presentation_mode() 
-
-        if task_index == 14:
-            self.speak("I will deactivate the presentation mode")
-            self.task_manager.finish_presentation()
-
-        
-        if task_index == 15:
-            # self.speak("I will open notepad")
-            self.task_manager.say_something()
-
-        if task_index == 16:
-            self.speak("Goodbye everyone")
+            self.task_manager.goodbye()
+            self.speak("Goodbye")
